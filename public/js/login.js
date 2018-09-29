1057976d8b99fe8d37c0e6d898fb5c659a5f7d71
qianning <liuqi@deakin.edu.au>
2018-09-30 00:09:24 +1000

    all front code

--- public/js/login.js
+++ public/js/login.js
@@ -3,36 +3,41 @@ function userLogin()
   if(checkLoginSubmit())
   {
 
+//connect database and get the array of the username and the password from database
     var email = $('#loginEmail').val();
     var password = $('#loginPassword').val();
 
-    $.ajax
-    ({
-        type:"get",
-        url:"php/checkUser.php",
-        async:true,
-        data:{lEmail:email, lPassword:password},
-        dataType : 'json',
-        success:function(res)
-        {
-          if (res == 1)
-          {
-            window.location.href='customer-orders.php';
-          }
-          else if(res ==2)
-          {
-            alert("Wrong password!")
-          }
-          else {
-            alert("users not exist!");
-            window.location.reload();
-          }
-        },
-        error:function(res)
-        {
-          alert(res);
-            console.log(res);
+    var dataEmail = [];
+    var dataPassword = [];
+
+    for(var i = 0; i<dataEmail.length; i++){
+      if(dataEmail[i] == email){
+        if(dataPassword[i] == password){
+          window.location.href = index.html;
         }
-      });
+        else{
+          alert("wrong password!")
+        }
+      }
+      else{
+        alert("wrong email! Please try again or sign up for your new account")
+      }
+    }
+  }
+}
+
+function checkLoginSubmit(){
+  var email = $('#loginEmail').val();
+  var password = $('#loginPassword').val();
+  if(email == null || email == ""){
+    document.getElementById("loginEmailError").innerHTML = "*Please write your email  ";
+    return false;
+  }
+  else if (password == null || password == ""){
+    document.getElementById("loginPasswordError").innerHTML = "*Please write your email  ";
+    return false;
+  }
+  else {
+    return true;
   }
 }
